e30cd0c143a37cd27b79567d97e6e5d310f266d2
zyk <1215182546@qq.com>
2023-06-28 01:40:00 +0800

    forth commit

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -22,6 +22,29 @@ function Toolbar(){
                         <svg t="1687790360741" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3707" width="18" height="18"><path d="M787.2 380.8c-9.6-9.6-22.4-12.8-35.2-12.8l-480 3.2c-12.8 0-25.6 3.2-35.2 12.8-19.2 19.2-19.2 48 0 67.2l240 240c0 0 0 0 0 0 0 0 0 0 0 0 3.2 3.2 9.6 6.4 12.8 9.6 0 0 3.2 3.2 3.2 3.2 16 6.4 38.4 3.2 51.2-9.6l240-243.2C806.4 428.8 803.2 400 787.2 380.8z" p-id="3708"></path></svg>
                     </span>
                 </div>
+                <div className={styles['dropdown-menu']}>
+                    <div className={styles['panel-h0']}>
+                        <div className={styles['content']}>正文</div>
+                    </div>
+                    <div className={styles['panel-h1']}>
+                        <div className={styles['content']}>标题一</div>
+                    </div>
+                    <div className={styles['panel-h2']}>
+                        <div className={styles['content']}>标题二</div>
+                    </div>
+                    <div className={styles['panel-h3']}>
+                        <div className={styles['content']}>标题三</div>
+                    </div>
+                    <div className={styles['panel-h4']}>
+                        <div className={styles['content']}>标题四</div>
+                    </div>
+                    <div className={styles['panel-h5']}>
+                        <div className={styles['content']}>标题五</div>
+                    </div>
+                    <div className={styles['panel-h6']}>
+                        <div className={styles['content']}>标题六</div>
+                    </div>
+                </div>
             </div>
             <div className={styles['cut-off-rule']}></div>
             <div className={styles['align-content']}>
